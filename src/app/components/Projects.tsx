66e6c7acf56bbe0aa50af113c2b974805addbaa6
surafelgk <118168857+Surafelgk@users.noreply.github.com>
2026-01-05 17:52:29 +0300

    Update Projects.tsx

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -26,7 +26,7 @@ const projects: Project[] = [
     Zure Addis is a digital marketing and creative agency based in Addis Ababa, Ethiopia. It focuses on helping businesses grow their brand online through services like digital strategy, social media marketing, content creation, branding, and design. The company positions itself as a modern marketing partner that builds engaging, audience-focused campaigns and visual identities tailored to local and global markets.
     `,
     tags: ["HTML","CSS", "Javascript", "Bootstrap", ],
-    video: "/12.mp4" autoplay muted loop></video>,
+    video: "/12.mp4",
     website: "",
     source: "https://github.com/surafelgk"
   },
@@ -44,7 +44,7 @@ This is my **first web development project**, where I built a Netflix-style inte
       "Javascript",
       
     ],
-    video: "/13.mp4"autoplay muted loop></video>,
+    video: "/13.mp4",
     website: "",
     source: "https://github.com/surafelgk",
   },
